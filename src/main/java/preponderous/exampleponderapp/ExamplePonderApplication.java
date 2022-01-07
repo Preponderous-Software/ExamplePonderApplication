f99515024138b37ed3fa9771d18b69fc2399b760
dmccoystephenson <dmccoystephenson@gmail.com>
2022-01-06 17:14:32 -0700

    Added comments to the main class.

--- src/main/java/preponderous/exampleponderapp/ExamplePonderApplication.java
+++ src/main/java/preponderous/exampleponderapp/ExamplePonderApplication.java
@@ -22,16 +22,28 @@ public class ExamplePonderApplication extends AbstractPonderApplication {
     private CommandService commandService;
     private final Scanner scanner = new Scanner(System.in);
 
+    /**
+     * This can be utitlized to access the self-managed instance of the application.
+     * @return The self-managed instance of the application.
+     */
     public static ExamplePonderApplication getInstance() {
         return instance;
     }
 
+    /**
+     * Initializes values and calls the onStartup method.
+     */
     public ExamplePonderApplication() {
         super("ExamplePonderApplication", "This is an example of an application created with Ponder.");
         onStartup();
     }
 
-    public boolean run(AbstractCommandSender sender) {
+    /**
+     * The primary method for the application. This is where the running loop can be found.
+     * @param user The user of the application.
+     * @return Whether the program exited successfully.
+     */
+    public boolean run(AbstractCommandSender user) {
         Logger.getInstance().log("Running application.");
 
         // declare variables to be used in loop
@@ -39,7 +51,7 @@ public class ExamplePonderApplication extends AbstractPonderApplication {
         String label;
         String[] args;
 
-        sender.sendMessage("Welcome to an example ponder application. Type help to see a list of useful commands.");
+        user.sendMessage("Welcome to an example ponder application. Type help to see a list of useful commands.");
         while (isRunning()) {
             line = getInput();
             if (line == null) {
@@ -60,7 +72,7 @@ public class ExamplePonderApplication extends AbstractPonderApplication {
             }
 
             // handle command
-            boolean success = onCommand(sender, label, args);
+            boolean success = onCommand(user, label, args);
             if (!success) {
                 Logger.getInstance().log("Something went wrong processing the " + label + " command.");
             }
@@ -68,14 +80,21 @@ public class ExamplePonderApplication extends AbstractPonderApplication {
         return true;
     }
 
+    /**
+     * This can be used to get input from the console.
+     * @return The inputted string.
+     */
     private String getInput() {
         // get input
-        if (!scanner.hasNext()) {
+        if (!getScanner().hasNext()) {
             return null;
         }
-        return scanner.nextLine();
+        return getScanner().nextLine();
     }
 
+    /**
+     * This will be called when the application starts up.
+     */
     @Override
     public void onStartup() {
         instance = this;
@@ -83,41 +102,98 @@ public class ExamplePonderApplication extends AbstractPonderApplication {
         initializeCommandService();
     }
 
+    /**
+     * This will be called when the application shuts down.
+     */
     @Override
     public void onShutdown() {
         Logger.getInstance().log("Initiating shutdown.");
     }
 
+    /**
+     * This will be called when a command is entered into the application.
+     * @param sender The sender of the command.
+     * @param label The label of the command.
+     * @param args The arguments of the command.
+     * @return Whether the execution of command was successful.
+     */
     @Override
     public boolean onCommand(AbstractCommandSender sender, String label, String[] args) {
         Logger.getInstance().log("Interpreting command " + label);
-        return commandService.interpretCommand(sender, label, args);
+        return getCommandService().interpretCommand(sender, label, args);
     }
 
+    /**
+     * This can be used to check whether the debug flag is enabled.
+     * @return Whether the debug flag is enabled.
+     */
     public boolean isDebugEnabled() {
         return debug;
     }
 
+    /**
+     * This can be used to set the debug flag to true or false.
+     * @param debug Desired value for the debug flag.
+     */
     public void setDebugEnabled(boolean debug) {
         this.debug = debug;
     }
 
+    /**
+     * This can be used to check if the application is running.
+     * @return Whether the application is running.
+     */
     public boolean isRunning() {
         return running;
     }
 
+    /**
+     * This can be used to set the running flag of the application.
+     * @param running Desired value for the running flag.
+     */
     public void setRunning(boolean running) {
         this.running = running;
     }
 
+    /**
+     * Initializes the command service with the application's commands.
+     */
     private void initializeCommandService() {
         HashSet<AbstractCommand> commands = new HashSet<>();
         commands.add(new HelpCommand());
         commands.add(new InfoCommand());
         commands.add(new QuitCommand());
-        commandService = new CommandService(commands);
+        setCommandService(new CommandService((commands)));
     }
 
+    /**
+     * This can be used to access Ponder's command service.
+     * @return This application's managed instance of Ponder's command service.
+     */
+    private CommandService getCommandService() {
+        return commandService;
+    }
+
+    /**
+     * This can be used to set this application's managed instance of Ponder's command service.
+     * @param commandService The instance of Ponder's command service to use.
+     */
+    private void setCommandService(CommandService commandService) {
+        this.commandService = commandService;
+    }
+
+    /**
+     * This can be used to access this application's managed scanner.
+     * @return This application's managed scanner.
+     */
+    private Scanner getScanner() {
+        return scanner;
+    }
+
+    /**
+     * Instantiates and runs the application.
+     * @param args The arguments given to the program.
+     */
     public static void main(String[] args) {
         ExamplePonderApplication application = new ExamplePonderApplication();
         CommandSender sender = new CommandSender();
